cfee5a7d54ddeac962d76623dd1e02d6e165b6cb
Nihat Eren Saltabas <122479699+saltabasnihat@users.noreply.github.com>
2023-07-20 01:35:07 -0400

    Add files via upload

---
+++ band_name_generator/practice.py
@@ -0,0 +1,13 @@
+print("Hello World!\nHow ya doing\ntoday!")  #input
+print("Hello " + input("What is your name?") + "!")
+# command / ---> comments all 
+
+print(len("Eren"))
+print(len(input("What is your name?")))
+
+# Write a program that prints the number of characters in a user's name.
+user_input = input("What is your name? ")
+print(len(user_input))    # print(len(input("What is your name? ")))
+
+
+
